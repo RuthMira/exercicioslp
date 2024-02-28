624fb456ff826604c7a1a4a405a587deb27818df
Ruth mira <143049504+RuthMira@users.noreply.github.com>
2024-02-28 20:06:38 -0300

    alterando calculoo da expressa

--- Tarefa1/MediaLP.java
+++ Tarefa1/MediaLP.java
@@ -23,19 +23,11 @@ public class MediaLP {
         System.out.print("Digite o valor de SUB: ");
         double SUB = scanner.nextDouble();
 
-        // Calcula a média das provas
-        double mediaProvas = (P1 * 0.6 + ((E1 + E2) / 2) * 0.4);
-
-        // Verifica se a média das provas é menor que 6
-        if (mediaProvas < 6) {
-            // Se a média das provas for menor que 6, a API não é considerada
-            API = 0;
-        }
-
         // Calcula a expressão
-        double extra = (API + mediaProvas > 10) ? 0 : Math.max((mediaProvas - 5.9), 0) / (mediaProvas - 5.9);
-
-        double resultado = (mediaProvas * 0.5 + extra) * (API * 0.5) + X + (SUB * 0.2);
+        double mediaProvas = (P1 * 0.6 + ((E1 + E2) / 2) * 0.4);
+        double extra = (API + mediaProvas > 10) ? 0 : Math.max((mediaProvas - 5.9), 0) / (5.9 - mediaProvas);
+        
+        double resultado = (mediaProvas * 0.5 + extra) * (API * 0.5) + X + (SUB * 0.2);        
 
         // Imprime o resultado
         System.out.println("O resultado da expressão é: " + resultado);
