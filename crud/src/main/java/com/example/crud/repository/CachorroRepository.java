8e609d2586ee55d8f70a9d82acd6d0dec3d185ec
ruthmira-1risjc <152284068+ruthmira-1risjc@users.noreply.github.com>
2024-06-12 09:19:16 -0300

    organizando pastas

--- crud/src/main/java/com/example/crud/repository/CachorroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.model.Cachorro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CachorroRepository extends JpaRepository<Cachorro, Long> {
-}
